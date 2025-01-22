5cea67803b7e15b39fee16a5e43b52468d41a26e
MrButier <uedemonlogic@gmail.com>
2025-01-22 07:28:18 -0800

    Remove Stupid Button error

--- Scripts/UI/spell_grid.gd
+++ Scripts/UI/spell_grid.gd
@@ -3,9 +3,12 @@ extends FullGrid
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if Engine.is_editor_hint(): return
+	
 	for child in get_children():
 		if child is ToggleButton:
-			child.button_toggled.connect(toggle_buttons)
+			var button : ToggleButton = child as ToggleButton
+			button.button_toggled.connect(toggle_buttons)
 
 func toggle_buttons(_color : GlobalSpells.SpellColor, value : bool):
 	print(value)
@@ -13,7 +16,6 @@ func toggle_buttons(_color : GlobalSpells.SpellColor, value : bool):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
 
 func check_cast_conditions() -> void:
 	GlobalSpells.get_spell_result(GlobalSpells.spells[1])
