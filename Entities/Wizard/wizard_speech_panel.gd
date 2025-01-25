789844b301fc17bc22ae3e2a8b84e6a8c38bdf1b
NathanTheCraziest <nathanthecraziest@gmail.com>
2025-01-25 20:03:57 +0530

    FixedSpeech

--- Entities/Wizard/wizard_speech_panel.gd
+++ Entities/Wizard/wizard_speech_panel.gd
@@ -2,6 +2,7 @@ extends Control
 
 class_name WizardSpeechPanel
 
+var target: String = ""
 var text: String = ""
 
 @onready var speech: Label = $Speech
@@ -11,9 +12,11 @@ func _ready() -> void:
 
 
 func set_speech(message: String):
+	target = message
 	text = ""
 	
 	for i in message:
-		text += i
-		await get_tree().create_timer(0.05).timeout
-		speech.text = text
+		if target == message:
+			text += i
+			await get_tree().create_timer(0.05).timeout
+			speech.text = text
