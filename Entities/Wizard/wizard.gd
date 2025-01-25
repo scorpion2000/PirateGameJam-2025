8743291983593f77e27513d18900001cf8020f9a
MrButier <uedemonlogic@gmail.com>
2025-01-25 01:14:10 -0800

    Added Damage indicator

---
+++ Entities/Wizard/wizard.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var intensity: float = 1.0
+@export var duration: float = 0.8
+
+func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
+	trigger_flash()
+
+func trigger_flash():
+	var player_shader = self.material
+	var time = 0.0
+	while time < duration:
+		var current_intensity = sin((PI * time) / duration) * intensity
+		player_shader.set_shader_parameter("flash_amount", current_intensity)
+		time += get_process_delta_time()
+		await get_tree().process_frame
+	player_shader.set_shader_parameter("flash_amount", 0.0)
+
+
+func _cast_result(result: bool) -> void:
+	if !result: trigger_flash()
